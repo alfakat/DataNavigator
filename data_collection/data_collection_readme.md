c952aea7bdfe7fe06b4ae265adc120ee8d5b932e
Kate Alfandari <44461784+alfakat@users.noreply.github.com>
2025-01-24 16:07:47 +0200

    Data collection update
    - created script for data generation
    - added readme

---
+++ data_collection/data_collection_readme.md
@@ -0,0 +1,9 @@
+**Data collecton** is process of obtainment the inputs (pictures for this specific case)
+
+There are different ways to collect data such as: 
+
+- _recording_ using phone or laptop camera;
+- _scrapping_ over the web pages;
+- _generating_ with help diffusion models.
+
+In this folder useful scripts to help collect data in those ways.
--- data_collection/data_scrapping.py
+++ data_collection/data_scrapping.py
@@ -2,12 +2,29 @@
 Data scrapping is one of the pioneer's data collection techniques and still wide useful.
 The following script navigates to Google search and download image by its url.
 '''
-import os.path
 
-from PIL import Image
-import requests
 import io
+import os.path
+import argparse
+import requests
+import datetime
+from PIL import Image
 from bs4 import BeautifulSoup
+
+
+def argument_parsing() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--prompt", type=str, required=True,
+                        help="Short prompt to search",
+                        default=0)
+    parser.add_argument("--output", type=str, required=False,
+                        help="Path where to save the scraped images",
+                        default=os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                             f'{str(datetime.date.today())}_scrap_output'))
+
+    args = parser.parse_args()
+    return args
 
 
 def save_url_image(link: str, id: int, output: str):
@@ -18,7 +35,7 @@ def save_url_image(link: str, id: int, output: str):
 
 
 def scrape_image_url(url: str, output: str):
-    '''navigate to Google search line'''
+
     page = requests.get(url).text
     bs = BeautifulSoup(page, 'html.parser')
 
@@ -34,8 +51,11 @@ def scrape_image_url(url: str, output: str):
 
 
 if __name__ == "__main__":
-    # to do: make search_key as list, add pages to search
-    search_key = 'cat'
-    output = r'C:\Users\KATE\Downloads\DataNavigator-main\data_collection\test'
-    scrape_image_url(url = rf'https://www.google.com/search?q={search_key}&sxsrf=ALeKk00uvzQYZFJo03cukIcMS-pcmmbuRQ:1589501547816&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjEm4LZyrTpAhWjhHIEHewPD1MQ_AUoAXoECBAQAw&biw=1440&bih=740',
+
+    args = argument_parsing()
+
+    prompt = args.prompt
+    output = args.output
+
+    scrape_image_url(url = rf'https://www.google.com/search?q={prompt}&sxsrf=ALeKk00uvzQYZFJo03cukIcMS-pcmmbuRQ:1589501547816&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjEm4LZyrTpAhWjhHIEHewPD1MQ_AUoAXoECBAQAw&biw=1440&bih=740',
                      output=output)
